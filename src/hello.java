6304d42f58510f5fba3c3276b8d62db595b1ddaf
silverlinker <443051056@qq.com>
2019-01-10 10:49:57 +0800

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Mingrui Zhao on 2019/1/10.
+ */
+public class hello {
+     public static void main(String [] args)
+     {
+         System.out.println("hello,IDEA");
+
+     }
+}
